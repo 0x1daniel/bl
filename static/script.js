330cec37319a37aa973f8c492757fece60344762
Daniel Oltmanns <oltmannsdaniel@outlook.com>
2018-10-22 12:22:30 +0200

    cookie banner: Fix path property
    Showed Cookie information will be now on the root path, not on a sub path if set.

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 /* Copyright 2018, Daniel Oltmanns (https://github.com/oltdaniel) */
 
 if(!document.cookie.includes('showed_cookie_banner=true')) {
-  document.getElementsByTagName('header')[0].innerHTML += '<cookie-banner><p>Cookies  will only be used for authentication purposes. No personal information will be stored nor given to other parties.</p> <u onclick="document.cookie += \'showed_cookie_banner=true;\'; this.parentNode.remove();">Hide</u></cookie-banner>'
+  document.getElementsByTagName('header')[0].innerHTML += '<cookie-banner><p>Cookies  will only be used for authentication purposes. No personal information will be stored nor given to other parties.</p> <u onclick="document.cookie += \'showed_cookie_banner=true;path=/;\'; this.parentNode.remove();">Hide</u></cookie-banner>'
 }
